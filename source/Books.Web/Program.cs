ceaca18a3691b0e60778486247b03aeadd8d18fc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-07-03 20:25:53 +0530

    Today's Work

--- source/Books.Web/Program.cs
+++ source/Books.Web/Program.cs
@@ -1,3 +1,4 @@
+using Books.Web.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -14,6 +15,9 @@ namespace Books.Web
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddHttpClient<IBookDataService, BookDataService>(client =>
+                client.BaseAddress = new Uri(builder.Configuration["WebApis:Books"]));
 
             await builder.Build().RunAsync();
         }
