d729f9361eaf0bb7d8e5ddd0f98b82a040752952
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-07-10 09:55:54 +0530

    Today's changes

---
+++ source/Books.Web/Pages/BookPages/EditBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Web.Pages.BookPages
+{
+    
+    public partial class EditBook
+    {
+    }
+
+}
