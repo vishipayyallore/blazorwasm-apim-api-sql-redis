6919f2efbd48f336bbea364eb11f01ccd7420493
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-07-10 10:10:29 +0530

    .NET 6 version of Web App

---
+++ source/Books.WebV1/Pages/BookPages/AddBook.cs
@@ -0,0 +1,38 @@
+﻿using Books.Data;
+using Books.DataServices;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace Books.WebV1.Pages.BookPages
+{
+
+    public partial class AddBook
+    {
+        [Inject]
+        private IBookDataService BookDataService { get; set; }
+
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
+        private readonly Random _random = new();
+
+        public Book NewBook { get; set; } = new Book();
+
+        protected async Task SaveBook()
+        {
+            NewBook.PictureUrl = $"/images/books/Book{_random.Next(1, 10)}.jpg";
+
+            await BookDataService.AddBook(NewBook);
+
+            NavigationManager.NavigateTo("bookslist");
+        }
+
+        protected void Home()
+        {
+            NavigationManager.NavigateTo("bookslist");
+        }
+
+    }
+
+}
---
+++ source/Books.WebV1/Pages/BookPages/EditBook.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.WebV1.Pages.BookPages
+{
+    public partial class EditBook
+    {
+    }
+}
