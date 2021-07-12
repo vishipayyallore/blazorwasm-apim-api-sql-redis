3de39e162727a17e5e97a6109528ce0b807140b6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-07-12 23:19:28 +0530

    Few Changes

--- source/Books.Web/Program.cs
+++ source/Books.Web/Program.cs
@@ -1,5 +1,8 @@
 using Books.DataServices;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
@@ -16,8 +19,28 @@ namespace Books.Web
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                builder.Configuration.Bind("Auth0", options.ProviderOptions);
+            });
+
             builder.Services.AddHttpClient<IBookDataService, BookDataService>(client =>
-                client.BaseAddress = new Uri(builder.Configuration["WebApis:Books"]));
+                client.BaseAddress = new Uri(builder.Configuration["WebApis:Books"]))
+                .AddHttpMessageHandler(sp =>
+                    {
+                        var provider = sp.GetRequiredService<IAccessTokenProvider>();
+                        var naviManager = sp.GetRequiredService<NavigationManager>();
+
+                        var handler = new AuthorizationMessageHandler(provider, naviManager);
+                        handler.ConfigureHandler(authorizedUrls: new[]
+                            {
+                                builder.Configuration["WebApis:Books"]
+                            }
+                        );
+
+                        return handler;
+                    }
+                );
 
             await builder.Build().RunAsync();
         }
